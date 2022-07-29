bdad0543fda550d3bae967c39fc3eeed5387d4db
rahneverd <rahneverd@gmail.com>
2022-07-30 02:19:43 +0500

    bug removed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import './App.css'
 import React, { Component } from 'react'
 
-export default class App extends Component {
+export class App extends Component {
   render() {
     return (
       <div>
-        Hello my first class based component
+        Hello there
       </div>
     )
   }
