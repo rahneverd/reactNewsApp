9727a56ff1e2c171afd96e399b886321cfd8785c
rahneverd <rahneverd@gmail.com>
2022-09-20 20:46:39 -0700

    port number changed

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,33 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-   module.exports = {
-       output: {
-           path: path.join(__dirname, '/dist'),
-           filename: 'index.bundle.js'
-       },
-       devServer: {
-           port: 3010,
-           historyApiFallback: true
-       },
-       module: {
-           rules: [
-               {
-                   test: /\.(js|jsx)$/,
-                   exclude: /node_modules/,
-                   use: {
-                       loader: 'babel-loader'
-                   }
-               },
-               {
-                   test: /\.(.css|scss)$/,
-                   use: [
-                       MiniCssExtractPlugin.loader,
-                       'css-loader',
-                       'sass-loader'
-                   ]
-               },
-               {
-                test: /\.(png|svg|jpg|jpeg|gif|tiff)$/,
-                use: [
-                    'file-loader?name=assets/[name].[ext]'
-                ]
-                }
-           ]
-       },
-       plugins: [new MiniCssExtractPlugin()]
-   }
+module.exports = {
+	output: {
+		path: path.join(__dirname, '/dist'),
+		filename: 'index.bundle.js',
+	},
+	devServer: {
+		port: process.env.PORT,
+		historyApiFallback: true,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+			{
+				test: /\.(.css|scss)$/,
+				use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif|tiff)$/,
+				use: ['file-loader?name=assets/[name].[ext]'],
+			},
+		],
+	},
+	plugins: [new MiniCssExtractPlugin()],
+};
